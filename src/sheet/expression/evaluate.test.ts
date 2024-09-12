9c2e76a84551bb84676974b1ba892f23b6ce6fbe
iyxan23 <nurihsanalghifari@gmail.com>
2024-09-12 20:27:46 +0700

    test: add a new test to test lambdas

--- src/sheet/expression/evaluate.test.ts
+++ src/sheet/expression/evaluate.test.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+import { createTemplaterFunction } from "../sheet-templater";
 import { evaluateExpression } from "./evaluate";
 import { Expression } from "./parser";
 import { success } from "./result";
@@ -185,7 +187,9 @@ describe("expression evaluation", () => {
       expr,
       { col: 0, row: 0, callTree: ["root"] },
       (fName) =>
-        fName === "hello" ? (_funcName, arg) => success("hello " + arg) : undefined,
+        fName === "hello"
+          ? (_funcName, arg) => success("hello " + arg)
+          : undefined,
       (_vName) => undefined,
     );
 
@@ -207,6 +211,61 @@ describe("expression evaluation", () => {
 
     expect(executionResult.issues).toHaveLength(0);
     expect(executionResult.result).toEqual("hello people!");
+    expect(consoleWarnMock).toHaveBeenCalledTimes(0);
+  });
+
+  it("evaluates a function that calls a lambda", (test) => {
+    const consoleWarnMock = mockWarn();
+    test.onTestFinished(() => consoleWarnMock.mockRestore());
+
+    // [call { [ret [:world]] }]
+    const expr: Expression = {
+      type: "call",
+      identifier: "call",
+      args: [
+        {
+          type: "lambda",
+          expression: {
+            type: "call",
+            identifier: "ret",
+            args: [
+              {
+                type: "variableAccess",
+                identifier: "world",
+                args: [],
+              },
+            ],
+          },
+        },
+      ],
+    };
+
+    const result = evaluateExpression(
+      expr,
+      { col: 0, row: 0, callTree: ["root"] },
+      (fName) =>
+        fName === "call"
+          ? createTemplaterFunction(z.tuple([z.function()]), (s) => {
+            return s((vName: string) =>
+              vName === "world" ? "people!" : undefined,
+            );
+          }).call
+          : fName === "ret"
+            ? createTemplaterFunction(z.tuple([z.string()]), (s) => {
+              return success(`ret ${s}`);
+            }).call
+            : undefined,
+      (_vName) => undefined,
+    );
+
+    if (result.status === "failed") {
+      throw new Error(JSON.stringify(result));
+    }
+
+    console.log(result.issues);
+
+    expect(result.issues).toHaveLength(0);
+    expect(result.result).toEqual("ret people!");
     expect(consoleWarnMock).toHaveBeenCalledTimes(0);
   });
 });
