748b3754772d1821a3fc802d1e16de7f382389b7
iyxan23 <nurihsanalghifari@gmail.com>
2024-09-14 22:31:17 +0700

    test: add a new gigantic complex test

--- src/sheet/sheet-templater.test.ts
+++ src/sheet/sheet-templater.test.ts
@@ -248,7 +248,7 @@ describe("SheetTemplater", () => {
     ]);
   });
 
-  it("do a repeatRow", () => {
+  it("does a repeatRow", () => {
     const sheet = new Sheet<TemplatableCell>([
       [cell("No."), cell("Full Name"), cell("Age"), cell("GPA")],
       [
@@ -307,6 +307,283 @@ describe("SheetTemplater", () => {
       [cell("2."), cell("Mark"), cell("93"), cell("100")],
       [cell("3."), cell("Elon"), cell("102"), cell("83")],
       [cell("4."), cell("Gates"), cell("83"), cell("73")],
+    ]);
+  });
+
+  it("does a repeatCol", () => {
+    const sheet = new Sheet<TemplatableCell>([
+      [cell("[#repeatCol [length [:students]] idx][add [:idx] 1].")],
+      [cell("[:students [:idx] fullName]")],
+      [cell("[:students [:idx] age]")],
+      [cell("[:students [:idx] gpa][/#repeatCol]")],
+    ]);
+
+    const templater = new SheetTemplater(sheet, {
+      functions: {
+        add: createTemplaterFunction(
+          z.tuple([z.any(), z.coerce.number()]),
+          (a, b) => success(a + b),
+        ),
+        length: createTemplaterFunction(z.tuple([z.array(z.any())]), (a) =>
+          success(a.length),
+        ),
+      },
+    });
+
+    const result = templater.interpret({
+      students: [
+        {
+          fullName: "John",
+          age: 57,
+          gpa: 21,
+        },
+        {
+          fullName: "Mark",
+          age: 93,
+          gpa: 100,
+        },
+        {
+          fullName: "Elon",
+          age: 102,
+          gpa: 83,
+        },
+        {
+          fullName: "Gates",
+          age: 83,
+          gpa: 73,
+        },
+        {
+          fullName: "Andrew",
+          age: 39,
+          gpa: 64,
+        },
+      ],
+    });
+
+    if (result.status === "failed") {
+      throw result.error;
+    }
+
+    expect(result.issues).toHaveLength(0);
+    expect(result.result.getSheet()).toEqual([
+      [cell("1."), cell("2."), cell("3."), cell("4."), cell("5.")],
+      [cell("John"), cell("Mark"), cell("Elon"), cell("Gates"), cell("Andrew")],
+      [cell("57"), cell("93"), cell("102"), cell("83"), cell("39")],
+      [cell("21"), cell("100"), cell("83"), cell("73"), cell("64")],
+    ]);
+  });
+
+  it("does a complex multiple blocks", () => {
+    const sheet = new Sheet<TemplatableCell>([
+      [
+        cell(
+          "[hoist grades [unique [flatten [map [:students] student { [keys [:students [:student] grades]] }]]]]",
+        ),
+      ],
+      [
+        cell("No"),
+        cell("Full Name"),
+        cell("Class"),
+        cell("[#repeatCol [:grades length] gradeIdx][:grades [:gradeIdx]]"),
+      ],
+      [
+        cell(
+          "[#repeatRow [:students length] studentIdx][add [:studentIdx] 1].",
+        ),
+        cell("[:students [:studentIdx] fullName]"),
+        cell("[:students [:studentIdx] class]"),
+        cell(
+          '[ifUndefined [:students [:studentIdx] grades [:grades [:gradeIdx]]] "N/A"][/#repeatCol][/#repeatRow]',
+        ),
+      ],
+    ]);
+
+    const templater = new SheetTemplater(sheet, {
+      functions: {
+        add: createTemplaterFunction(
+          z.tuple([z.coerce.number(), z.coerce.number()]),
+          (a, b) => success(a + b),
+        ),
+        unique: createTemplaterFunction(z.tuple([z.array(z.any())]), (a) =>
+          success([...new Set(a)]),
+        ),
+        keys: createTemplaterFunction(z.tuple([z.any()]), (obj) =>
+          success(Object.keys(obj)),
+        ),
+        flatten: createTemplaterFunction(
+          z.tuple([z.array(z.array(z.any()))]),
+          (arr) => success(arr.flat()),
+        ),
+        ifUndefined: createTemplaterFunction(
+          z.tuple([z.any(), z.string()]),
+          (a, b) => (a === undefined ? success(b) : success(a)),
+        ),
+        map: createTemplaterFunction(
+          z.tuple([z.array(z.any()), z.string(), z.function()]),
+          (arr, idxName, fn) => {
+            const resultArr = [];
+            const issues = [];
+            const callFn = callLambda(fn);
+
+            for (let i = 0; i < arr.length; i++) {
+              const result = callFn({
+                variables: {
+                  [idxName]: i,
+                },
+              });
+
+              if (result.status === "failed") return result;
+
+              issues.push(...result.issues);
+              resultArr.push(result.result);
+            }
+
+            return success(resultArr, issues);
+          },
+        ),
+      },
+    });
+
+    const result = templater.interpret({
+      students: [
+        {
+          fullName: "John",
+          class: "5A",
+          grades: {
+            math: 89,
+            english: 90,
+            programming: 110,
+            science: 93,
+          },
+        },
+        {
+          fullName: "Mark",
+          class: "5C",
+          grades: {
+            math: 89,
+            english: 90,
+            programming: 110,
+            science: 93,
+          },
+        },
+        {
+          fullName: "Elon",
+          class: "5B",
+          grades: {
+            math: 74,
+            spanish: 83,
+            social: 94,
+            science: 99,
+          },
+        },
+        {
+          fullName: "Gates",
+          class: "4B",
+          grades: {
+            math: 74,
+            german: 90,
+            programming: 70,
+            sport: 89,
+          },
+        },
+        {
+          fullName: "Andrew",
+          class: "4A",
+          grades: {
+            sport: 64,
+            english: 90,
+            german: 84,
+            social: 100,
+          },
+        },
+      ],
+    });
+
+    if (result.status === "failed") {
+      throw result.error;
+    }
+
+    expect(result.issues).toHaveLength(0);
+    expect(result.result.getSheet()).toEqual([
+      [cell("")],
+      [
+        cell("No"),
+        cell("Full Name"),
+        cell("Class"),
+        cell("math"),
+        cell("english"),
+        cell("programming"),
+        cell("science"),
+        cell("spanish"),
+        cell("social"),
+        cell("german"),
+        cell("sport"),
+      ],
+      [
+        cell("1."),
+        cell("John"),
+        cell("5A"),
+        cell("89"),
+        cell("90"),
+        cell("110"),
+        cell("93"),
+        cell("N/A"),
+        cell("N/A"),
+        cell("N/A"),
+        cell("N/A"),
+      ],
+      [
+        cell("2."),
+        cell("Mark"),
+        cell("5C"),
+        cell("89"),
+        cell("90"),
+        cell("110"),
+        cell("93"),
+        cell("N/A"),
+        cell("N/A"),
+        cell("N/A"),
+        cell("N/A"),
+      ],
+      [
+        cell("3."),
+        cell("Elon"),
+        cell("5B"),
+        cell("74"),
+        cell("N/A"),
+        cell("N/A"),
+        cell("99"),
+        cell("83"),
+        cell("94"),
+        cell("N/A"),
+        cell("N/A"),
+      ],
+      [
+        cell("4."),
+        cell("Gates"),
+        cell("4B"),
+        cell("74"),
+        cell("N/A"),
+        cell("70"),
+        cell("N/A"),
+        cell("N/A"),
+        cell("N/A"),
+        cell("90"),
+        cell("89"),
+      ],
+      [
+        cell("5."),
+        cell("Andrew"),
+        cell("4A"),
+        cell("N/A"),
+        cell("90"),
+        cell("N/A"),
+        cell("N/A"),
+        cell("N/A"),
+        cell("100"),
+        cell("84"),
+        cell("64"),
+      ],
     ]);
   });
 });
