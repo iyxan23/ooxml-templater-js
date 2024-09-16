e54ea5210dca773bc421565f3cbb78995d281154
iyxan23 <nurihsanalghifari@gmail.com>
2024-09-16 22:33:40 +0700

    chore: set vite base to "/ooxml-templater-js/"

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ooxml-templater-js/",
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
