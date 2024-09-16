4b5d64e5a46202cce087c51db334b557cdb71721
iyxan23 <nurihsanalghifari@gmail.com>
2024-09-16 22:42:26 +0700

    fix: set basename of react router to ooxml-templater-js

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -9,24 +9,29 @@ import { ThemeProvider } from "./components/theme-provider";
 import XlsxPage from "./routes/xlsx/page";
 import DocxPage from "./routes/docx/page";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootRoute />,
+      errorElement: <ErrorPage />,
+      children: [
+        { index: true, element: <IndexPage /> },
+        {
+          path: "xlsx",
+          element: <XlsxPage />,
+        },
+        {
+          path: "docx",
+          element: <DocxPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <RootRoute />,
-    errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <IndexPage /> },
-      {
-        path: "xlsx",
-        element: <XlsxPage />
-      },
-      {
-        path: "docx",
-        element: <DocxPage />
-      }
-    ],
+    basename: "/ooxml-templater-js/",
   },
-]);
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
