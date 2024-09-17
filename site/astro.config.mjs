564d06810697fa992cadea0a1717f2afe393935b
iyxan23 <nurihsanalghifari@gmail.com>
2024-09-17 09:49:21 +0700

    fix: set site base to ooxml-templater-js

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,6 +5,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/ooxml-templater-js/',
+  trailingSlash: "always",
   integrations: [
     tailwind({
       applyBaseStyles: false,
