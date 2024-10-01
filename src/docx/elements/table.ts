5bd15b0f3f4d558551b507c96962becdee233291
iyxan23 <nurihsanalghifari@gmail.com>
2024-10-01 16:17:43 +0700

    feat: impl parsing w:tr on table

--- src/docx/elements/table.ts
+++ src/docx/elements/table.ts
@@ -1,6 +1,7 @@
 import { Result, success } from "../../result";
 import { BodyElement } from ".";
 import { DocAddr } from "../doc-templater";
+import { startVisiting } from "src/visitor-editor";
 
 const W_TBL_PR = "w:tblPr";
 const W_TBL_GRID = "w:tblGrid";
@@ -11,7 +12,7 @@ const W_TR = "w:tr";
 export class TableElement implements BodyElement {
   private tblPr: any[] | null = null;
   private tblGrid: GridCols | null = null;
-  private rows: any[] = [];
+  private rows: TableRow[] = [];
   private other: any[] = [];
 
   constructor(private obj: any) {
@@ -24,7 +25,7 @@ export class TableElement implements BodyElement {
       } else if (keys.includes(W_TBL_GRID)) {
         this.tblGrid = new GridCols(node[W_TBL_GRID]);
       } else if (keys.includes(W_TR)) {
-        this.rows.push(node[W_TR]);
+        this.rows.push(new TableRow(node[W_TR]));
       } else {
         this.other.push(node);
       }
@@ -49,12 +50,99 @@ export class TableElement implements BodyElement {
   }
 }
 
+class TableRow {
+  public cells: TableCell[] = [];
+  private other: any[] = [];
+
+  constructor(rawTr: any[]) {
+    for (const node of rawTr) {
+      const keys = Object.keys(node);
+      if (keys.includes("w:tc")) {
+        this.cells.push(node["w:tc"]);
+      } else {
+        this.other.push(node);
+      }
+    }
+  }
+
+  rebuild(): any[] {
+    return [...this.cells.map((c) => ({ "w:tc": c.rebuild() })), ...this.other];
+  }
+}
+
+class TableCell {
+  public w: number;
+  private textPath: string[] | null;
+
+  constructor(private raw: any) {
+    let w;
+    for (const node of raw) {
+      const nodeKeys = Object.keys(node);
+
+      if (nodeKeys.includes("w:tcPr")) {
+        const tcPr = node["w:tcPr"];
+        for (const tcPrItem of tcPr) {
+          const tcPrItemKeys = Object.keys(tcPrItem);
+          if (tcPrItemKeys.includes("w:tcW")) {
+            w = parseInt(tcPrItem["w:tcW"][":@"]["@_w:w"]);
+          }
+        }
+      }
+    }
+
+    if (!w) throw new Error("unable to find width of cell");
+
+    this.w = w;
+
+    // find text
+    let textPath: string[] | null = null;
+
+    startVisiting(raw, {
+      before: {
+        "w:t": [
+          (_node, path) => {
+            textPath = path;
+            return {};
+          },
+        ],
+      },
+      after: {},
+    });
+
+    this.textPath = textPath ?? null;
+  }
+
+  get text(): string | null {
+    if (!this.textPath) return null;
+
+    let cur = this.raw;
+    for (const p of this.textPath) cur = cur[p];
+
+    const textNode = cur.find((c: any) => Object.keys(c).includes("#text"));
+    return textNode["#text"];
+  }
+
+  set text(newText: string) {
+    if (!this.textPath) return;
+
+    let cur = this.raw;
+    for (const p of this.textPath) cur = cur[p];
+
+    const textNode = cur.find((c: any) => Object.keys(c).includes("#text"));
+    textNode["#text"] = newText;
+  }
+
+  rebuild(): any[] {
+    return this.raw;
+  }
+}
+
 class GridCols {
   private cols: GridCol[] = [];
   private other: any[] = [];
 
-  constructor(private rawTblGrid: any[]) {
-    for (const node of this.rawTblGrid) {
+  constructor(rawTblGrid: any[]) {
+    for (const node of rawTblGrid) {
       const keys = Object.keys(node);
       if (keys.includes("w:gridCol")) {
         this.cols.push(new GridCol(node["w:gridCol"]));
