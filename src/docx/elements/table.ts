fcc4e637ca9deb876399865ffc207b2d38729d75
iyxan23 <nurihsanalghifari@gmail.com>
2024-10-02 17:36:48 +0700

    feat: mostly implement special call extraction on docx table

--- src/docx/elements/table.ts
+++ src/docx/elements/table.ts
@@ -1,8 +1,9 @@
-import { Issue, Result, success } from "../../result";
+import { failure, Issue, Result, success } from "../../result";
 import { BodyElement } from ".";
 import { DocAddr } from "../doc-templater";
 import { startVisiting } from "../../visitor-editor";
 import {
+  BasicExpression,
   BasicExpressionsWithStaticTexts,
   parseBasicExpressions,
 } from "../../expression/parser";
@@ -11,11 +12,23 @@ import {
   TemplaterFunction,
 } from "../../expression/evaluate";
 import { isNumeric } from "../../utils";
+import { Expressionish, extract, Source } from "../../expression/extractor";
 
 const W_TBL_PR = "w:tblPr";
 const W_TBL_GRID = "w:tblGrid";
 
 const W_TR = "w:tr";
+
+type RepeatRow = {
+  row: number;
+  startCol: number;
+  endCol: number;
+
+  innerRepeats: RepeatRow[];
+
+  count: BasicExpression | string;
+  idxIdent: string;
+};
 
 // @internal
 export class TableElement implements BodyElement {
@@ -41,6 +54,139 @@ export class TableElement implements BodyElement {
     }
   }
 
+  private extractSpecialCalls(
+    source: TableRowsSource,
+    context: { addr: TableAddr },
+  ): Result<RepeatRow[], TableAddr> {
+    const issues: Issue<TableAddr>[] = [];
+    const repeatRows: RepeatRow[] = [];
+
+    function handleRepeatRow(
+      starting: TableAddr,
+      expr: Extract<BasicExpression, { type: "specialCall" }>,
+    ): RepeatRow | null {
+      if (expr.args.length !== 2) {
+        issues.push({
+          message: `r#repeatRow takes two arguments: [r#repeatRow [count] idx]`,
+          addr: context.addr,
+        });
+      }
+
+      const repeatRowCountArg = expr.args[0]!;
+      const repeatRowIdxArg = expr.args[1]!;
+
+      if (typeof repeatRowIdxArg !== "string") {
+        issues.push({
+          message: `invalid second argument of r#repeatRow: ${repeatRowIdxArg} (must be a text for identifier)`,
+          addr: context.addr,
+        });
+
+        return null;
+      }
+
+      let closing: TableAddr | null = null;
+      const innerRepeatRows: RepeatRow[] = [];
+
+      extract<TableAddr, TableCell>(
+        source,
+        {
+          visitSpecialCall(addr, _item, expr) {
+            if (expr.code !== "r" || expr.identifier !== "repeatRow") {
+              issues.push({
+                message: `Unknown special call [${expr.code}#${expr.identifier}]`,
+                addr: context.addr,
+              });
+
+              return { deleteExpr: true };
+            }
+
+            if (!expr.closing) {
+              const result = handleRepeatRow(
+                { col: addr.col + 1, row: addr.row },
+                expr,
+              );
+
+              // todo: do something
+              if (!result) return { deleteExpr: true };
+
+              innerRepeatRows.push(result);
+            } else {
+              closing = { col: addr.col, row: addr.row };
+            }
+
+            return { deleteExpr: true };
+          },
+        },
+        starting,
+        ({ col: prevCol, row: prevRow }) => {
+          if (prevCol + 1 >= source.getMaxColumns()) return null;
+          return {
+            col: prevCol + 1,
+            row: prevRow,
+          };
+        },
+      );
+
+      if (closing == null) {
+        issues.push({
+          message: `r#repeatRow at col ${starting.col + 1}, row ${starting.row + 1} is not closed`,
+          addr: context.addr,
+        });
+
+        return null;
+      }
+
+      return {
+        row: starting.row,
+        startCol: starting.col,
+        // @ts-expect-error false positive, closing is not `never`
+        endCol: closing.col,
+        innerRepeats: innerRepeatRows,
+        count: repeatRowCountArg,
+        idxIdent: repeatRowIdxArg,
+      };
+    }
+
+    extract<TableAddr, TableCell>(
+      source,
+      {
+        visitSpecialCall(addr, _item, expr) {
+          if (expr.code !== "r" || expr.identifier !== "repeatRow") {
+            issues.push({
+              message: `Unknown special call [${expr.code}#${expr.identifier}]`,
+              addr: context.addr,
+            });
+
+            return { deleteExpr: true };
+          }
+
+          const repeatRow = handleRepeatRow(
+            { col: addr.col + 1, row: addr.row },
+            expr,
+          );
+
+          if (repeatRow != null) repeatRows.push(repeatRow);
+
+          return { deleteExpr: true };
+        },
+      },
+      { col: 0, row: 0 },
+      ({ col: prevCol, row: prevRow }) => {
+        if (prevCol + 1 >= source.getMaxColumns())
+          return {
+            col: 0,
+            row: prevRow + 1,
+          };
+        return {
+          col: prevCol + 1,
+          row: prevRow,
+        };
+      },
+    );
+
+    return success(repeatRows, issues);
+  }
+
   expand(
     context: {
       addr: DocAddr;
@@ -49,131 +195,67 @@ export class TableElement implements BodyElement {
     getVariable: (name: string) => any,
     getFunction: (name: string) => TemplaterFunction<any, DocAddr> | undefined,
   ): Result<void, DocAddr> {
+    console.log("EXPANDING TABLE");
     const issues: Issue<DocAddr>[] = [];
-    const repeatedRows: Record<
-      number,
-      { count: number; ident: string; start: number; end: number }[]
-    > = {};
+    const source = new TableRowsSource(this.rows);
 
-    for (let rowIdx = 0; rowIdx < this.rows.length; rowIdx++) {
-      const row = this.rows[rowIdx]!;
-      let currentRepeatRow: {
-        col: number;
-        count: number;
-        ident: string;
-      } | null = null;
+    const extractionResult = this.extractSpecialCalls(source, {
+      addr: { col: 0, row: 0 },
+    });
 
-      cell: for (let col = 0; col < row.cells.length; col++) {
-        const cell = row.cells[col]!;
-
-        if (!cell.parsedExpr) continue cell;
-
-        expr: for (let i = 0; i < cell.parsedExpr.length; i++) {
-          const item = cell.parsedExpr[i];
-          if (typeof item !== "object" || item.type !== "specialCall")
-            continue expr;
-
-          if (item.code !== "r" || item.identifier !== "repeatRow") {
-            issues.push({
-              message: `Unknown special call [${item.code}#${item.identifier}]`,
-              addr: context.addr,
-            });
-
-            continue expr;
-          }
-
-          if (item.closing) {
-            if (!currentRepeatRow) {
-              issues.push({
-                message: `closing [/r#repeatRow] with no opening`,
-                addr: context.addr,
-              });
-
-              continue expr;
-            }
-
-            // complete, opening closed
-            if (repeatedRows[rowIdx] == undefined) repeatedRows[rowIdx] = [];
-
-            repeatedRows[rowIdx]!.push({
-              count: currentRepeatRow.count,
-              ident: currentRepeatRow.ident,
-              start: currentRepeatRow.col,
-              end: col,
-            });
-
-            currentRepeatRow = null;
-
-            continue expr;
-          } else {
-            if (!!currentRepeatRow) {
-              // opening with already opened
-              issues.push({
-                message: `r#repeatRow cannot be used more than once on the same row at row ${rowIdx}`,
-                addr: context.addr,
-              });
-
-              continue expr;
-            }
-          }
-
-          const repeatLineCountArg = item.args[0];
-          const repeatLineIdxArg = item.args[1];
-
-          let repeatLineCount;
-          if (typeof repeatLineCountArg !== "string") {
-            const evalResult = evaluateExpression<DocAddr>(
-              item,
-              context,
-              getFunction,
-              getVariable,
-            );
-
-            if (evalResult.status === "failed") return evalResult;
-            issues.push(...evalResult.issues);
-
-            repeatLineCount = evalResult.result;
-          }
-
-          if (
-            typeof repeatLineCount === "string" &&
-            isNumeric(repeatLineCount)
-          ) {
-            repeatLineCount = Number(repeatLineCount);
-          } else {
-            issues.push({
-              message: `invalid first argument of l#repeatLine: ${repeatLineCountArg} (must be a number, or evaluate to a number)`,
-              addr: context.addr,
-            });
-
-            continue expr;
-          }
-
-          if (typeof repeatLineIdxArg !== "string") {
-            issues.push({
-              message: `invalid second argument of l#repeatLine: ${repeatLineIdxArg} (must be a text for identifier)`,
-              addr: context.addr,
-            });
-
-            continue expr;
-          }
-
-          currentRepeatRow = {
-            col,
-            count: repeatLineCount,
-            ident: repeatLineIdxArg,
-          };
-        }
-      }
+    if (extractionResult.status === "failed") {
+      return failure(
+        { ...extractionResult.error, addr: context.addr },
+        extractionResult.issues.map((ei) => ({ ...ei, addr: context.addr })),
+      );
     }
 
-    for (const [rowKey, repeatRow] of Object.entries(repeatedRows)) {
-      const rowNum = Number(rowKey);
-      for (const r of repeatRow) {
-        const row = this.rows[rowNum]!.cells.slice(r.start, r.end);
-        const clonedRow = row.map((c) => new TableRow(c.rebuild()));
-        this.rows.splice(rowNum, 0, ...clonedRow);
+    issues.push(
+      ...extractionResult.issues.map((ei) => ({ ...ei, addr: context.addr })),
+    );
+
+    const repeatRows = extractionResult.result;
+
+    for (const repeatRow of repeatRows) {
+      const repeatRowCountArg = repeatRow.count;
+      const repeatRowIdxArg = repeatRow.idxIdent;
+
+      let repeatRowCount;
+      if (typeof repeatRowCountArg !== "string") {
+        const evalResult = evaluateExpression<DocAddr>(
+          repeatRowCountArg,
+          context,
+          getFunction,
+          getVariable,
+        );
+
+        if (evalResult.status === "failed") return evalResult;
+        issues.push(...evalResult.issues);
+
+        repeatRowCount = evalResult.result;
       }
+
+      if (typeof repeatRowCount === "string" && isNumeric(repeatRowCount)) {
+        repeatRowCount = Number(repeatRowCount);
+      } else {
+        issues.push({
+          message: `invalid first argument of r#repeatRow: ${repeatRowCountArg} (must be a number, or evaluate to a number), at table col ${repeatRow.startCol + 1} row ${repeatRow.row + 1}`,
+          addr: context.addr,
+        });
+
+        continue;
+      }
+
+      if (typeof repeatRowIdxArg !== "string") {
+        issues.push({
+          message: `invalid second argument of r#repeatRow: ${repeatRowIdxArg} (must be a string), at table col ${repeatRow.startCol + 1} row ${repeatRow.row + 1}`,
+          addr: context.addr,
+        });
+
+        continue;
+      }
+
+      // todo: repeat this row `repeatRowCount` times
     }
 
     return success(undefined, issues);
@@ -216,6 +298,31 @@ export class TableElement implements BodyElement {
   }
 }
 
+type TableAddr = { col: number; row: number };
+
+class TableRowsSource implements Source<TableAddr, TableCell> {
+  constructor(private rows: TableRow[]) { }
+
+  getItem(addr: TableAddr): TableCell | null {
+    const { col, row } = addr;
+    return this.rows[row]?.cells[col] ?? null;
+  }
+
+  setItem(addr: TableAddr, item: TableCell): void {
+    const { col, row } = addr;
+    if (!this.rows[row]) {
+      console.warn("[TableRowsSource] trying to setItem when no row at", row);
+      return;
+    }
+
+    this.rows[row].cells[col] = item;
+  }
+
+  getMaxColumns(): number {
+    return this.rows[0]?.cells.length ?? 0;
+  }
+}
+
 class TableRow {
   public cells: TableCell[] = [];
   private other: any[] = [];
@@ -236,7 +343,7 @@ class TableRow {
   }
 }
 
-class TableCell {
+class TableCell implements Expressionish {
   public w: number;
   private textPath: string[] | null;
   public parsedExpr: BasicExpressionsWithStaticTexts | null;
@@ -339,6 +446,50 @@ class TableCell {
 
   rebuild(): any {
     return this.raw;
+  }
+
+  // -- implements Expressionish
+
+  getExpression(): BasicExpressionsWithStaticTexts {
+    return this.parsedExpr ?? [];
+  }
+
+  removeExpression(index: number): void {
+    if (!this.parsedExpr) {
+      console.warn(
+        "[TableCell] trying to removeExpression at",
+        index,
+        "when no parsedExpr",
+      );
+
+      return;
+    }
+
+    this.parsedExpr.splice(index, 1);
+
+    // check if the items on the left and right are strings, if so, combine
+    // them together
+    const prev = this.parsedExpr[index - 1];
+    const next = this.parsedExpr[index];
+    if (typeof prev === "string" && typeof next === "string") {
+      this.parsedExpr[index - 1] = prev + this.parsedExpr[index];
+      this.parsedExpr.splice(index, 1);
+      return;
+    }
+  }
+
+  replaceExpression(expr: BasicExpression, index: number): void {
+    if (!this.parsedExpr) {
+      console.warn(
+        "[TableCell] trying to replaceExpression at",
+        index,
+        "when no parsedExpr",
+      );
+
+      return;
+    }
+
+    this.parsedExpr[index] = expr;
   }
 }
 
